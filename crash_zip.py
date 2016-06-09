09ee65461b3932a7145959f4d74370ffe865d488
rocky chen <jinweizsu@139.com>
2016-06-09 21:29:00 +0800

    initial v1.0

--- crash_zip.py
+++ crash_zip.py
@@ -1,17 +1,26 @@
 __author__ = 'rocky'
 import zipfile
+
 filename="sample.zip"
+
 dictFile="pwdict.txt"
+
 password=open(dictFile,'r')
+
 for p in password:
+
     zf=zipfile.ZipFile(filename)
+
     p=p.strip('\n')
-    print p
+
     try:
         zf.extractall("./sample",pwd=p)
+
         print "crash. Password is %s" %p
+
         exit(0)
     except:
         pass
 
-print "No found password"
+
+
